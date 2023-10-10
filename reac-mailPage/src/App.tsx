da526c6bc8c650762634f83600910a8fcdcdce9f
Matheus Santos <matheus.santos@fitbank.com.br>
2023-10-10 17:23:43 -0300

    second commit

--- reac-mailPage/src/App.tsx
+++ reac-mailPage/src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
 
+import NeutraLogo from './assets/NeutraLogo.svg'
+import Card from './components/Card/Card'
+
 function App() {
   
 
   return (
     <div className='app'>
-      Hello world
+      <div className="head">
+        <img src={NeutraLogo} alt="" />
+        Neutra+
+      </div>
+      <Card />
+      <div className="contact">
+        
+      </div>
+      <div className="footer">
+
+      </div>
     </div>
   )
 }
--- reac-mailPage/src/components/Page/Page.tsx
+++ reac-mailPage/src/components/Page/Page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div className='container'>
+    <div className='page'>
         <div>Page</div>
         <div>Page</div>
         <div>Page</div>
